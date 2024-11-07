6e7287d62bafddcb98d429c7a67a2de4f6bf6b90
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2024-11-08 01:22:13 +0700

    update appjs

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
-import HomeScreen from "./layout/HomeScreen";
-import Header from "./layout/Header";
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import RegisterScreen from './layout/RegisterScreen';
-import LoginScreen from './layout/LoginScreen';
-import ProfileScreen from './layout/ProfileScreen';
-import WritingScreen from './layout/WritingScreen';
-import PostScreen from './layout/PostScreen';
-import TopicScreen from './layout/TopicScreen';
-
-=======
 import Header from "./layout/Header";
 import HomeScreen from "./layout/HomeScreen";
 import LoginScreen from './layout/LoginScreen';
 import RegisterScreen from './layout/RegisterScreen';
 import TopicScreen from './layout/TopicScreen';
->>>>>>> Phuc
 
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
-      <Header/>
-      <>
-      <Route path="/topic" component={TopicScreen}/>
-      <Route path="/post" component={PostScreen}/>
-      <Route path="/writing" component={WritingScreen}/>
-      <Route path="/profile" component={ProfileScreen}/>
-      <Route path="/login" component={LoginScreen}/>
-      <Route path="/register" component={RegisterScreen}/>
-      
-      <Route path="/" component ={HomeScreen}/>
-      </>
-=======
       <Header />
       <Routes>
         <Route path="/" element={<HomeScreen />} />
@@ -43,7 +17,6 @@ function App() {
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/register" element={<RegisterScreen />} />
       </Routes>
->>>>>>> Phuc
     </Router>
   );
 }
